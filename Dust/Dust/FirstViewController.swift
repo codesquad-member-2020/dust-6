62094e4ba1ed74773e305f690545d15328a03bfe
TTOzzi <devttozzi@gmail.com>
2020-03-30 15:28:20 +0900

    Feat: 프로젝트 생성
    Tabbed App 으로 프로젝트 생성
    사용하지 않는 코드 제거

---
+++ Dust/Dust/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Dust/Dust/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
