0e78d259d996d75e4bc409df6faf28f7224bc0dd
TTOzzi <devttozzi@gmail.com>
2020-03-30 22:01:46 +0900

    Feat: 미세먼지 농도 그래프 TableView 추가

---
+++ iOS/Dust/Dust/View/DustDensityTableView.swift
@@ -0,0 +1,19 @@
+//
+//  DustDensityTableView.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class DustDensityTableView: UITableView {
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
