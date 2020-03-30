56d128d846e2a05266f26bcb5b93a1bcc6e923c3
TTOzzi <devttozzi@gmail.com>
2020-03-30 15:53:57 +0900

    Feat: StatusView 커스텀클래스 생성
    스토리보드에서 상단 UI 를 담을 View 를 만들어줌
    오토레이아웃으로 View 의 위치를 잡아주고 높이를 SafeArea 의 45% 로 지정
    UIView 를 상속받은 StatusView 클래스를 만들어 상단 View 의 커스텀클래스로 지정
    잘 되었는지 확인을 위한 배경색 지정

---
+++ iOS/Dust/Dust/View/StatusView.swift
@@ -0,0 +1,25 @@
+//
+//  StatusView.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class StatusView: UIView {    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    func setProperties() {
+        self.backgroundColor = .red
+    }
+}
