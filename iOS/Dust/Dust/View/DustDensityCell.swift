731c0df56791d3ff171718bb5ff30da4e4f37020
TTOzzi <devttozzi@gmail.com>
2020-03-30 23:52:19 +0900

    Feat: 바 그래프 값에따른 색상변화 구현 Resolve #4
    전달받은 값에 따라 알맞은 색상을 바에 적용
    switch-case 를 쓰지 않고 dictionary 로 분기를 나눠주기위해 범위를 키값으로 쓸 수 있도록 dictionary 를 확장함
    셀이 재사용 될 때 바가 계속 남게되어 바가 존재할때 지워주는 메소드 추가

--- iOS/Dust/Dust/View/DustDensityCell.swift
+++ iOS/Dust/Dust/View/DustDensityCell.swift
@@ -12,13 +12,26 @@ class DustDensityCell: UITableViewCell {
     static let identifier = "DustDensityCell"
     
     func setBarWidth(with value: Int) {
+        if !contentView.subviews.isEmpty {
+            removeSubviews()
+        }
         let fillWidth = (CGFloat(value) / 200) * self.frame.size.width
         let rect = CGRect(x: 0, y: 0, width: fillWidth, height: self.frame.size.height)
         let bar = UIView(frame: rect)
-        bar.backgroundColor = .red
         self.contentView.addSubview(bar)
         self.textLabel?.text = "\(value)"
         self.textLabel?.textAlignment = .right
         self.textLabel?.font = .systemFont(ofSize: 12.0)
+        let gradeScope: [ClosedRange<Int>:UIColor] = [0...30:.systemBlue,
+                                                      31...80:.systemGreen,
+                                                      81...120:.systemOrange,
+                                                      121...9999:.systemRed]
+        bar.backgroundColor = gradeScope[value]
+    }
+    
+    func removeSubviews() {
+        self.contentView.subviews.forEach {
+            $0.removeFromSuperview()
+        }
     }
 }
