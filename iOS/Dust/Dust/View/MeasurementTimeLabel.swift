360c011935103a3850ac4bf6cf65c928b77052d6
TTOzzi <devttozzi@gmail.com>
2020-03-30 19:45:43 +0900

    Feat: 최근 측정 시각 표시 Label 추가

---
+++ iOS/Dust/Dust/View/MeasurementTimeLabel.swift
@@ -0,0 +1,27 @@
+//
+//  MeasurementTimeLabel.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class MeasurementTimeLabel: UILabel {
+    var time = "오늘 19:00"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    func setProperties() {
+        self.text = time
+    }
+}
