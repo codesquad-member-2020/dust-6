130b1faaa24b08127482d92faf978838a2db6260
TTOzzi <devttozzi@gmail.com>
2020-03-30 17:44:09 +0900

    Feat: 상태 이모지 Label 추가
    StatusEmoji 커스텀클래스 생성
    등급별 이모티콘을 열거형으로 나눠줌

---
+++ iOS/Dust/Dust/View/StatusEmoji.swift
@@ -0,0 +1,32 @@
+//
+//  StatusEmoji.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/30.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import UIKit
+
+class StatusEmoji: UILabel {
+    enum State: String {
+        case good = "😀"
+        case normal = "🙂"
+        case bad = "😷"
+        case worst = "😱"
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setProperties()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setProperties()
+    }
+    
+    func setProperties() {
+        self.text = State.good.rawValue
+    }
+}
