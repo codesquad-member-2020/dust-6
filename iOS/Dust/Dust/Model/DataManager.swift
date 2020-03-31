fc6a3fe5228aa770c16d62e0b3dea653408aa05f
TTOzzi <devttozzi@gmail.com>
2020-03-31 14:56:49 +0900

    Feat: 데이터를 저장 할 모델 구현
    받아올 JSON 의 모양대로 임시 구조체를 만들고 테스트를 위한 임의값이 담긴 구조체도 함께 만들어줌
    데이터를 관리하는 DataManger 객체 생성

---
+++ iOS/Dust/Dust/Model/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  Dust
+//
+//  Created by TTOzzi on 2020/03/31.
+//  Copyright © 2020 TTOzzi. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+    var dustData = MockDustStatus()
+}
