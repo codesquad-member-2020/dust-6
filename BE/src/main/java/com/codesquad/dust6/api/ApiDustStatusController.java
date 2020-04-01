ab81315a728fde35184d8477b520c3d05ccf0509
ever <hsik0225@gmail.com>
2020-04-02 02:49:12 +0900

    Refactor : 위도, 경도 클래스 생성

--- BE/src/main/java/com/codesquad/dust6/api/ApiDustStatusController.java
+++ BE/src/main/java/com/codesquad/dust6/api/ApiDustStatusController.java
@@ -1,11 +1,9 @@
 package com.codesquad.dust6.api;
 
+import com.codesquad.dust6.domain.Coordinate;
 import com.codesquad.dust6.domain.Location;
 import com.codesquad.dust6.domain.ResponseDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,8 +13,8 @@ public class ApiDustStatusController {
 
     //tm 좌표 기준 가장 가까운 측정소 목록
     @GetMapping("/locations")
-    public String locations(@RequestParam("latitude") float latitude, @RequestParam("longitude") float longitude) {
-        return "redirect:/공공api/측정소목록";
+    public float locations(Coordinate coordinate) {
+        return coordinate.getLongitude();
     }
 
     @GetMapping("/공공api/측정소목록")
@@ -26,15 +24,16 @@ public class ApiDustStatusController {
 
     //측정소 목록 중 가장 가까운 측정소 1개
     @GetMapping("location")
-    private Location location(@RequestParam("latitude") float latitude, @RequestParam("longitude") float longitude) {
-        locations(latitude, longitude);
+    private Location location(Coordinate coordinate) {
+//        locations(latitude, longitude);
         return null;
     }
 
     //측정소의 미세먼지 상태
     @GetMapping("")
-    public ResponseDTO getStatus(@RequestParam("latitude") float latitude, @RequestParam("longitude") float longitude) {
-        String stationName = location(latitude, longitude).getStationName();
-        return "redirect:/공공api/측정소_이름";
+    public ResponseDTO getStatus(Coordinate coordinate) {
+        String stationName = location(coordinate).getStationName();
+        return null;
+//        return "redirect:/공공api/측정소_이름";
     }
 }
---
+++ BE/src/main/java/com/codesquad/dust6/domain/Coordinate.java
@@ -0,0 +1,22 @@
+package com.codesquad.dust6.domain;
+
+public class Coordinate {
+    private float latitude;
+    private float longitude;
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
