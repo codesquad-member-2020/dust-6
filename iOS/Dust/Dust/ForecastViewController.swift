735fcf706b2b2333f09e60c9433f473e8378a84b
TTOzzi <devttozzi@gmail.com>
2020-04-03 00:42:36 +0900

    Feat: Notification 으로 넘겨받은 데이터를 이미지로 바꿔 화면에 출력하는 메소드 구현 Resolve #48
    2초마다 화면이 바뀜
    재생버튼 누르면 화면이 바뀌도록 수정 필요

--- iOS/Dust/Dust/ForecastViewController.swift
+++ iOS/Dust/Dust/ForecastViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class ForecastViewController: UIViewController {
     @IBOutlet weak var forecastOverall: UILabel!
     @IBOutlet weak var forecastGrade: UILabel!
+    @IBOutlet weak var forecastImage: UIImageView!
     
     private var dataManager = ForecastDataManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dataManager.setDustStatusData()
-        NotificationCenter.default.addObserver(self, selector: #selector(updateViews(_:)), name: ForecastDataManager.dataDidload, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateViews(_:)), name: ForecastDataManager.dataDidLoad, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateForecastImage(_:)), name: ForecastDataManager.imageDidLoad, object: nil)
     }
     
     @objc private func updateViews(_ notification: NSNotification) {
@@ -26,6 +28,13 @@ class ForecastViewController: UIViewController {
         DispatchQueue.main.async {
             self.updateOverall(overall)
             self.updateGrade(grade)
+        }
+    }
+    
+    @objc private func updateForecastImage(_ notification: NSNotification) {
+        guard let data = notification.userInfo?[ForecastDataManager.image] as? Data else { return }
+        DispatchQueue.main.async {
+            self.forecastImage.image = UIImage(data: data)
         }
     }
     
