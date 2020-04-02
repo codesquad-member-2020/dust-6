c93d9ffccf851b14f765e20fcad71a16ec7f8d07
suhyunsim <pcn021152@gmail.com>
2020-04-02 20:29:22 +0900

    Feat: JSON 받아오는 메소드 다른 클래스로 이동

---
+++ BE/src/main/java/com/codesquad/dust6/utils/JsonUtils.java
@@ -0,0 +1,14 @@
+package com.codesquad.dust6.utils;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class JsonUtils {
+    private static final RestTemplate restTemplate = new RestTemplate();
+    public static String data(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        return restTemplate.getForObject(uri, String.class);
+    }
+}
