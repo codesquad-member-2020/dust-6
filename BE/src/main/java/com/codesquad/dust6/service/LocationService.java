158d942f1f8de8f16bfc5ada4f7c6be78cf58e3c
ever <hsik0225@gmail.com>
2020-04-02 15:08:38 +0900

    Feat : TM 좌표 기준 측정소 목록 구현

--- BE/src/main/java/com/codesquad/dust6/service/LocationService.java
+++ BE/src/main/java/com/codesquad/dust6/service/LocationService.java
@@ -1,12 +1,10 @@
 package com.codesquad.dust6.service;
 
+import com.codesquad.dust6.domain.DistanceDTO;
 import com.codesquad.dust6.utils.PublicInstitutionUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +17,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocationService {
 
@@ -28,19 +28,22 @@ public class LocationService {
 
     private static Logger logger = LoggerFactory.getLogger(LocationService.class);
 
-    public static Object test() throws URISyntaxException, JsonProcessingException, ParseException {
-        URI uri = new URI(PublicInstitutionUtils.URL + "?tmX=244148.546388&tmY=412423.75772" + PublicInstitutionUtils.SERVICE_KEY_AND_RETURN_TYPE);
-        String json = restTemplate.getForObject(uri, String.class);
+    public static List<DistanceDTO> test() throws URISyntaxException {
+        URI uri = new URI(PublicInstitutionUtils.URL + "tmX=244148.546388&tmY=412423.75772" + PublicInstitutionUtils.SERVICE_KEY_AND_RETURN_TYPE);
+        String data = restTemplate.getForObject(uri, String.class);
 
-        JSONParser jsonParser = new JSONParser();
+        List<DistanceDTO> distances = new ArrayList<>();;
 
-        //JSONParse에 JSON 데이터를 넣어 파싱한 다음 JSONObject로 변환한다
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
+        JSONArray jsonArray = new JSONObject(data).getJSONArray("list");
 
-        //JSONObjet에서 List를 Get하여 JSONArray에 저장한다
-        JSONArray distances = (JSONArray) jsonObject.get("list");
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String stationName = jsonObject.get("stationName").toString();
+            float tm = Float.parseFloat(jsonObject.get("tm").toString());
+            distances.add(new DistanceDTO(stationName, tm));
+        }
 
-        return distances.toJSONString();
+        return distances;
     }
 
 
