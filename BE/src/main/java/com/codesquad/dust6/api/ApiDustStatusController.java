57a5e5cfb4d60a9007a013852f2afe8a9882589c
ever <hsik0225@gmail.com>
2020-04-03 16:10:16 +0900

    Refactor : 사용하지 않는 클래스 삭제

--- BE/src/main/java/com/codesquad/dust6/api/ApiDustStatusController.java
+++ BE/src/main/java/com/codesquad/dust6/api/ApiDustStatusController.java
@@ -1,36 +1,35 @@
 package com.codesquad.dust6.api;
+
 import com.codesquad.dust6.domain.CoordinateDTO;
+import com.codesquad.dust6.domain.DistanceDTO;
 import com.codesquad.dust6.domain.ResponseDTO;
 import com.codesquad.dust6.service.LocationService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RequestMapping("/api/dust-status")
 @RestController
 public class ApiDustStatusController {
 
-    private Logger logger = LoggerFactory.getLogger(ApiDustStatusController.class);
-
     //tm 좌표 기준 가장 가까운 측정소 목록
     @GetMapping("/locations")
-    public Object locations(CoordinateDTO coordinate) throws IOException, URISyntaxException {
+    public List<DistanceDTO> locations(CoordinateDTO coordinate) throws IOException, URISyntaxException {
         CoordinateDTO tmCoordinate = LocationService.coordinate(coordinate);
-        logger.debug("tmCoordinate : {}", tmCoordinate);
         return LocationService.distances(tmCoordinate);
     }
-  
+
     //측정소 목록 중 가장 가까운 측정소 1개
     public String location(CoordinateDTO coordinate) throws IOException, URISyntaxException {
         CoordinateDTO tmCoordinate = LocationService.coordinate(coordinate);
         return LocationService.distance(tmCoordinate).getStationName();
     }
-  
+
     //측정소의 미세먼지 상태
     @GetMapping("location")
     public ResponseDTO getStatus(CoordinateDTO coordinate) throws URISyntaxException, IOException {
