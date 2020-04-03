e783de491820624c29cab5ebd799dbc4337ffab6
Reese Kim <reesekimm@gmail.com>
2020-04-04 03:16:31 +0900

    Feat: 상단 네비게이션 활성화 표시 기능 추가

--- FE/src/js/controllers/NavController.js
+++ FE/src/js/controllers/NavController.js
@@ -1,9 +1,23 @@
+import { $listen } from "Utils/utilFunction";
+
 const header = document.querySelector("header");
 const navDust = header.querySelector(".menu:first-child");
 const navForecast = header.querySelector(".menu:last-child");
 const container = document.querySelector(".container");
 
-navDust.addEventListener("touchstart", () => (container.style.left = 0), { passive: false });
-navForecast.addEventListener("touchstart", () => (container.style.left = "-100vw"), {
-	passive: false
-});
+const pageSwitchingHandler = e => {
+	const selectedMenu = e.target.dataset.title;
+	if (!selectedMenu) return;
+
+	if (selectedMenu === "미세먼지") {
+		container.style.left = 0;
+		navDust.classList.add("selected");
+		navForecast.classList.remove("selected");
+	} else {
+		container.style.left = "-100vw";
+		navForecast.classList.add("selected");
+		navDust.classList.remove("selected");
+	}
+};
+
+$listen(header, "touchend", e => pageSwitchingHandler(e));
