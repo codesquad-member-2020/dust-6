6e5b461a3d9573d0e4e3e0caf43a992b5bafbd7d
suhyunsim <pcn021152@gmail.com>
2020-04-04 13:21:21 +0900

    Refactor: 오전 12시-5시 api 요청 수정
    api 예보가 나오지 않는 오전 시간대의 예보 이미지를 전날 예보로 대체한다.

--- BE/src/main/java/com/codesquad/dust6/api/ApiForecastController.java
+++ BE/src/main/java/com/codesquad/dust6/api/ApiForecastController.java
@@ -2,16 +2,16 @@ package com.codesquad.dust6.api;
 
 import com.codesquad.dust6.domain.OverallDTO;
 import com.codesquad.dust6.domain.ResponseDTO;
-import com.codesquad.dust6.utils.JsonUtils;
-import com.codesquad.dust6.utils.PublicInstitutionUtils;
-import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +23,11 @@ public class ApiForecastController {
 
     @GetMapping("/api/dust/forecast")
     public ResponseDTO forecast() throws URISyntaxException {
-        String today = LocalDate.now().toString();
+        LocalTime currentTime = LocalTime.now();
+        String today;
+        if (currentTime.getHour() < 5) today = LocalDate.now().minusDays(1).toString();
+        else today = LocalDate.now().toString();
+
         String url = BASE_URL + FORECAST_URL
                 + "searchDate=" + today + SERVICE_KEY_AND_RETURN_TYPE;
         String response = data(url);
